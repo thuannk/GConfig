1615c0fc3d0b4131763902af38c41fa368b1a08f
ThuanNK <thuannk.91@gmail.com>
2016-03-23 11:40:09 +0700

    better checking

--- CounterStrike/check.bat
+++ CounterStrike/check.bat
@@ -1,4 +1,5 @@
-echo N|comp "C:\Program Files (x86)\Steam\userdata\91736926\730\local\cfg\autoexec.cfg" autoexec.cfg
-echo N|comp "C:\Program Files (x86)\Steam\userdata\91736926\730\local\cfg\custom.cfg" custom.cfg
-echo N|comp "C:\Program Files (x86)\Steam\userdata\91736926\730\local\cfg\video.txt" video.txt
+echo off
+echo N|comp "C:\Program Files (x86)\Steam\userdata\91736926\730\local\cfg\autoexec.cfg" autoexec.cfg 2>nul| find "OK"
+echo N|comp "C:\Program Files (x86)\Steam\userdata\91736926\730\local\cfg\custom.cfg" custom.cfg 2>nul| find "OK"
+echo N|comp "C:\Program Files (x86)\Steam\userdata\91736926\730\local\cfg\video.txt" video.txt 2>nul| find "OK"
 pause
