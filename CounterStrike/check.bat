8212f1aaaebb1f4d41985347c6aaf9fd2d46754f
ThuanNK <thuannk.91@gmail.com>
2016-04-09 10:46:49 +0700

    Remove check video.txt

--- CounterStrike/check.bat
+++ CounterStrike/check.bat
@@ -1,5 +1,4 @@
 echo off
 echo N|comp "C:\Program Files (x86)\Steam\userdata\91736926\730\local\cfg\autoexec.cfg" autoexec.cfg 2>nul| find "OK"
 echo N|comp "C:\Program Files (x86)\Steam\userdata\91736926\730\local\cfg\custom.cfg" custom.cfg 2>nul| find "OK"
-echo N|comp "C:\Program Files (x86)\Steam\userdata\91736926\730\local\cfg\video.txt" video.txt 2>nul| find "OK"
 pause
