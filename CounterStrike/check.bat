4e4e6d7a918fd88f38a7a434ae19ddc3eceab239
ThuanNK <thuannk.91@gmail.com>
2016-09-10 17:20:58 +0700

    Exec

--- CounterStrike/check.bat
+++ CounterStrike/check.bat
@@ -1,4 +1,4 @@
 echo off
 echo N|comp "C:\Program Files (x86)\Steam\userdata\91736926\730\local\cfg\autoexec.cfg" autoexec.cfg 2>nul| find "OK"
-echo N|comp "C:\Program Files (x86)\Steam\userdata\91736926\730\local\cfg\custom.cfg" custom.cfg 2>nul| find "OK"
+echo N|comp "C:\Program Files (x86)\Steam\userdata\91736926\730\local\cfg\fallen.cfg" fallen.cfg 2>nul| find "OK"
 pause
