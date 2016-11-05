2ab1e8d836b085f9352a35cc9820933b6eb1fd14
ThuanNK <thuannk.91@gmail.com>
2016-11-05 21:45:28 +0700

    Change name

--- CounterStrike/check.bat
+++ CounterStrike/check.bat
@@ -1,3 +1,3 @@
-comp "C:\Program Files (x86)\Steam\userdata\91736926\730\local\cfg\autoexec.cfg" autoexec.cfg
-comp "C:\Program Files (x86)\Steam\userdata\91736926\730\local\cfg\pro.cfg" pro.cfg
+echo N|comp "C:\Program Files (x86)\Steam\userdata\91736926\730\local\cfg\autoexec.cfg" autoexec.cfg
+echo N|comp "C:\Program Files (x86)\Steam\userdata\91736926\730\local\cfg\pro.cfg" pro.cfg
 pause
