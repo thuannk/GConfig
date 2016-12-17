8d30af0f4e43be9bb9ff6d068e62c74cc5c68d42
ThuanNK <thuannk.91@gmail.com>
2016-12-17 08:46:31 +0700

    Remove pro

--- CounterStrike/check.bat
+++ CounterStrike/check.bat
@@ -1,3 +1,2 @@
 echo N|comp "C:\Program Files (x86)\Steam\userdata\91736926\730\local\cfg\autoexec.cfg" autoexec.cfg
-echo N|comp "C:\Program Files (x86)\Steam\userdata\91736926\730\local\cfg\pro.cfg" pro.cfg
 pause
