32c691a3bdac20a181c222250ec616bbe413c1da
ThuanNK <thuannk.91@gmail.com>
2017-02-25 13:15:47 +0700

    Remove zip file, add launch bat and video config

--- CounterStrike/check.bat
+++
@@ -1,2 +0,0 @@
-echo N|comp "C:\Program Files (x86)\Steam\userdata\91736926\730\local\cfg\autoexec.cfg" autoexec.cfg
-pause
---
+++ CounterStrike/csgo.bat
@@ -0,0 +1,16 @@
+:: Replace autoexec and video.txt
+xcopy /V /Y /R autoexec.cfg "C:\Program Files (x86)\Steam\userdata\91736926\730\local\cfg\autoexec.cfg"
+xcopy /V /Y /R video.txt "C:\Program Files (x86)\Steam\userdata\91736926\730\local\cfg\video.txt"
+
+:: Delete old config
+del /F "C:\Program Files (x86)\Steam\userdata\91736926\730\local\cfg\config.cfg"
+
+:: Create new config with content is "exec autoexec"
+echo exec autoexec > "C:\Program Files (x86)\Steam\userdata\91736926\730\local\cfg\config.cfg"
+
+:: Check script
+echo Press Enter to start
+pause
+
+:: Start game
+start steam://rungameid/730
