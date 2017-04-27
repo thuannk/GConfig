b2246db2c19cf22007b6109575f783b459ad94d7
thuannk <thuannk.91@gmail.com>
2017-04-27 18:56:02 +0900

    Update

--- CounterStrike/csgo.bat
+++ CounterStrike/csgo.bat
@@ -1,6 +1,6 @@
 :: Replace autoexec and video.txt
 xcopy /V /Y /R autoexec.cfg "C:\Program Files (x86)\Steam\userdata\91736926\730\local\cfg\autoexec.cfg"
-xcopy /V /Y /R video.txt "C:\Program Files (x86)\Steam\userdata\91736926\730\local\cfg\video.txt"
+:: xcopy /V /Y /R video.txt "C:\Program Files (x86)\Steam\userdata\91736926\730\local\cfg\video.txt"
 
 :: Delete old config
 del /F "C:\Program Files (x86)\Steam\userdata\91736926\730\local\cfg\config.cfg"
@@ -9,7 +9,6 @@ del /F "C:\Program Files (x86)\Steam\userdata\91736926\730\local\cfg\config.cfg"
 echo exec autoexec > "C:\Program Files (x86)\Steam\userdata\91736926\730\local\cfg\config.cfg"
 
 :: Check script
-echo Press Enter to start
 pause
 
 :: Start game
