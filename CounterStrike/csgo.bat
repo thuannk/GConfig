b69052257241668bb5045e736ddc63a2fbd877e1
thuannk <thuannk.91@gmail.com>
2018-04-03 18:19:12 +0900

    Update new user ID

--- CounterStrike/csgo.bat
+++ CounterStrike/csgo.bat
@@ -1,13 +1,13 @@
 :: Replace autoexec and video.txt
-xcopy /V /Y /R autoexec.cfg "C:\Program Files (x86)\Steam\userdata\91736926\730\local\cfg\autoexec.cfg"
-xcopy /V /Y /R maplist.cfg "C:\Program Files (x86)\Steam\userdata\91736926\730\local\cfg\maplist.cfg"
-:: xcopy /V /Y /R video.txt "C:\Program Files (x86)\Steam\userdata\91736926\730\local\cfg\video.txt"
+xcopy /V /Y /R autoexec.cfg "C:\Program Files (x86)\Steam\userdata\867255386\730\local\cfg\autoexec.cfg"
+xcopy /V /Y /R maplist.cfg "C:\Program Files (x86)\Steam\userdata\867255386\730\local\cfg\maplist.cfg"
+:: xcopy /V /Y /R video.txt "C:\Program Files (x86)\Steam\userdata\867255386\730\local\cfg\video.txt"
 
 :: Delete old config
-del /F "C:\Program Files (x86)\Steam\userdata\91736926\730\local\cfg\config.cfg"
+del /F "C:\Program Files (x86)\Steam\userdata\867255386\730\local\cfg\config.cfg"
 
 :: Create new config with content is "exec autoexec"
-echo exec autoexec > "C:\Program Files (x86)\Steam\userdata\91736926\730\local\cfg\config.cfg"
+echo exec autoexec > "C:\Program Files (x86)\Steam\userdata\867255386\730\local\cfg\config.cfg"
 
 :: Check script
 pause
