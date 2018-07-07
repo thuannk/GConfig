509be8d4dc6a79dd0b46179e2de715bad10215bf
thuannk <thuannk.91@gmail.com>
2018-07-07 17:02:53 +0900

    New update configuration

---
+++ CounterStrike/csgo-reset.bat
@@ -0,0 +1,2 @@
+rd /S /Q "C:\Program Files (x86)\Steam\userdata\867981618\730\local\cfg"
+mkdir "C:\Program Files (x86)\Steam\userdata\867981618\730\local\cfg"
---
+++ CounterStrike/csgo-start.bat
@@ -0,0 +1,3 @@
+xcopy /V /Y /R autoexec.cfg "C:\Program Files (x86)\Steam\userdata\867981618\730\local\cfg\autoexec.cfg"
+xcopy /V /Y /R maplist.cfg "C:\Program Files (x86)\Steam\userdata\867981618\730\local\cfg\maplist.cfg"
+start steam://rungameid/730
