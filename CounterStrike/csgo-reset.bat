0341f7f24ce0f4379f3ad76077686a2671a083c6
thuannk <thuannk.91@gmail.com>
2018-07-11 20:31:01 +0900

    Update script

--- CounterStrike/csgo-reset.bat
+++ CounterStrike/csgo-reset.bat
@@ -1,2 +1,21 @@
-rd /S /Q "C:\Program Files (x86)\Steam\userdata\867981618\730\local\cfg"
-mkdir "C:\Program Files (x86)\Steam\userdata\867981618\730\local\cfg"
+@echo off
+SET CFOLDER="C:\Program Files (x86)\Steam\userdata\867981618\730\local\cfg"
+choice /C 12 /M "Press 1 to delete config.cfg. Press 2 to clear all cfg folder."
+if errorlevel 255 goto end
+if errorlevel 2 goto 2
+if errorlevel 1 goto 1
+if errorlevel 0 goto end
+:1
+echo Deleting config.cfg
+del /F %CFOLDER%\config.cfg
+goto end
+
+:2
+echo Clearing cfg folder
+rd /S /Q %CFOLDER%
+mkdir %CFOLDER%
+goto end
+
+:end
+echo Done!
+pause
--- CounterStrike/csgo-start.bat
+++ CounterStrike/csgo-start.bat
@@ -1,3 +1,4 @@
-xcopy /V /Y /R autoexec.cfg "C:\Program Files (x86)\Steam\userdata\867981618\730\local\cfg\autoexec.cfg"
-xcopy /V /Y /R maplist.cfg "C:\Program Files (x86)\Steam\userdata\867981618\730\local\cfg\maplist.cfg"
+SET CFOLDER="C:\Program Files (x86)\Steam\userdata\867981618\730\local\cfg"
+xcopy /V /Y /R autoexec.cfg %CFOLDER%
+xcopy /V /Y /R maplist.cfg %CFOLDER%
 start steam://rungameid/730
